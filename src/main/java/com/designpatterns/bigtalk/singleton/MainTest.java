6cc8f3a78b3051b7206ed49d96ed6fed5641252e
bingo <18845649698@163.com>
2021-03-25 01:51:41 +0800

    singleton

---
+++ src/main/java/com/designpatterns/bigtalk/singleton/MainTest.java
@@ -0,0 +1,13 @@
+package com.designpatterns.bigtalk.singleton;
+
+/**
+ * @author: jiangjiabin
+ * @date: Create in 1:09 2021/3/25
+ * @description:
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        SingletonLazy instance = SingletonLazy.getInstance();
+    }
+}
---
+++ src/main/java/com/designpatterns/bigtalk/singleton/SingletonLazy.java
@@ -0,0 +1,40 @@
+package com.designpatterns.bigtalk.singleton;
+
+/**
+ * @author: jiangjiabin
+ * @date: Create in 1:04 2021/3/25
+ * @description: 单例
+ * 懒汉是延迟实例化，是线程不安全的
+ * 这里实现 双重检查 的单例模式
+ * 能在多线程情况下保证高性能
+ *
+ * volatile，可以禁止指令重排
+ */
+public class SingletonLazy {
+
+    private static volatile SingletonLazy singletonLazy;
+
+    private SingletonLazy() {
+
+    }
+
+    /**
+     * new SingletonLazy() 不是原子的：
+     *  1、分配空间给对象 -> 2、在空间内创建对象 -> 3、将对象复制给引用instance
+     * 如果发生了指令重排，线程执行顺序是 1 -> 3 -> 2，会将值写进主内存，其他线程就会读到instance的最新值，
+     * 但是这是一个不完全的对象
+     * @return
+     */
+    public static SingletonLazy getInstance() {
+        if (singletonLazy == null) {
+            synchronized (SingletonLazy.class) {
+                if (singletonLazy == null) {
+                    singletonLazy = new SingletonLazy();
+                }
+            }
+        }
+        return singletonLazy;
+    }
+
+
+}
