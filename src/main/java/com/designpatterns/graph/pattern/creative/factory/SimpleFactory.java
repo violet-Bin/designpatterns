8eb266f1c38bbd830239efe5b0a570e5c795880e
bingo <18845649698@163.com>
2021-10-29 01:29:08 +0800

    factory

---
+++ src/main/java/com/designpatterns/graph/pattern/creative/factory/SimpleFactory.java
@@ -0,0 +1,9 @@
+package com.designpatterns.graph.pattern.creative.factory;
+
+/**
+ * @author: jiangjiabin
+ * @date: Create in 1:28 2021/10/29
+ * @description:
+ */
+public class SimpleFactory {
+}
